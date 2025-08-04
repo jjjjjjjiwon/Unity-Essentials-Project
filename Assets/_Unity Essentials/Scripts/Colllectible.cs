574d020b11974ee5baa94fbab2606b649ca9849b
jjjjjjjiwon <hgw7501@gmail.com>
2025-08-04 15:51:33 +0900

    0804

--- Assets/_Unity Essentials/Scripts/Colllectible.cs
+++ Assets/_Unity Essentials/Scripts/Colllectible.cs
@@ -6,15 +6,28 @@ public class Colllectible : MonoBehaviour
 {
     public float rotationSpeed;
 
+    public GameObject OnCollectEffect;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, rotationSpeed, 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Destroy the collectible;
+            Destroy(gameObject);
+            // Instantiate particle effect
+            Instantiate(OnCollectEffect, transform.position, transform.rotation);
+        }
     }
 }
